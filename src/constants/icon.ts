7b7ca9ce9ab34b14de0bc0784f96932fc27a512c
kinho <kinho6119@163.com>
2024-07-28 20:39:06 +0800

    更改icon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,36 +2,60 @@ import type {Favicon} from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: '/favicon/favicon-light-32.png',
-        theme: 'light',
-        sizes: '32x32',
-    }, {
-        src: '/favicon/favicon-light-128.png',
-        theme: 'light',
-        sizes: '128x128',
-    }, {
-        src: '/favicon/favicon-light-180.png',
-        theme: 'light',
-        sizes: '180x180',
-    }, {
-        src: '/favicon/favicon-light-192.png',
-        theme: 'light',
-        sizes: '192x192',
-    }, {
-        src: '/favicon/favicon-dark-32.png',
+        src: '/favicon/gundam-512.png',
         theme: 'dark',
-        sizes: '32x32',
+        sizes: '512x512',
     }, {
-        src: '/favicon/favicon-dark-128.png',
+        src: '/favicon/gundam-256.png',
+        theme: 'dark',
+        sizes: '256x256',
+    }, {
+        src: '/favicon/gundam-128.png',
         theme: 'dark',
         sizes: '128x128',
     }, {
-        src: '/favicon/favicon-dark-180.png',
+        src: '/favicon/gundam-64.png',
         theme: 'dark',
-        sizes: '180x180',
+        sizes: '64x64',
     }, {
-        src: '/favicon/favicon-dark-192.png',
+        src: '/favicon/gundam-32.png',
         theme: 'dark',
-        sizes: '192x192',
+        sizes: '32x32',
+    }, {
+        src: '/favicon/gundam-24.png',
+        theme: 'dark',
+        sizes: '24x24',
+    }, {
+        src: '/favicon/gundam-16.png',
+        theme: 'dark',
+        sizes: '16x16',
+    }, {
+        src: '/favicon/gundam-512.png',
+        theme: 'light',
+        sizes: '512x512',
+    }, {
+        src: '/favicon/gundam-256.png',
+        theme: 'light',
+        sizes: '256x256',
+    }, {
+        src: '/favicon/gundam-128.png',
+        theme: 'light',
+        sizes: '128x128',
+    }, {
+        src: '/favicon/gundam-64.png',
+        theme: 'light',
+        sizes: '64x64',
+    }, {
+        src: '/favicon/gundam-32.png',
+        theme: 'light',
+        sizes: '32x32',
+    }, {
+        src: '/favicon/gundam-24.png',
+        theme: 'light',
+        sizes: '24x24',
+    }, {
+        src: '/favicon/gundam-16.png',
+        theme: 'light',
+        sizes: '16x16',
     }
 ]
