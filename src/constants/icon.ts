a118888470d429f113b69eff27f19b83bd68a237
kinho <kinho6119@163.com>
2024-07-29 23:29:46 +0800

    更换icon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,59 +2,51 @@ import type {Favicon} from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: '/favicon/gundam-512.png',
+        src: '/favicon/sword-512.png',
         theme: 'dark',
         sizes: '512x512',
     }, {
-        src: '/favicon/gundam-256.png',
+        src: '/favicon/sword-256.png',
         theme: 'dark',
         sizes: '256x256',
     }, {
-        src: '/favicon/gundam-128.png',
+        src: '/favicon/sword-128.png',
         theme: 'dark',
         sizes: '128x128',
     }, {
-        src: '/favicon/gundam-64.png',
+        src: '/favicon/sword-64.png',
         theme: 'dark',
         sizes: '64x64',
     }, {
-        src: '/favicon/gundam-32.png',
+        src: '/favicon/sword-32.png',
         theme: 'dark',
         sizes: '32x32',
     }, {
-        src: '/favicon/gundam-24.png',
-        theme: 'dark',
-        sizes: '24x24',
-    }, {
-        src: '/favicon/gundam-16.png',
+        src: '/favicon/sword-16.png',
         theme: 'dark',
         sizes: '16x16',
     }, {
-        src: '/favicon/gundam-512.png',
+        src: '/favicon/sword-512.png',
         theme: 'light',
         sizes: '512x512',
     }, {
-        src: '/favicon/gundam-256.png',
+        src: '/favicon/sword-256.png',
         theme: 'light',
         sizes: '256x256',
     }, {
-        src: '/favicon/gundam-128.png',
+        src: '/favicon/sword-128.png',
         theme: 'light',
         sizes: '128x128',
     }, {
-        src: '/favicon/gundam-64.png',
+        src: '/favicon/sword-64.png',
         theme: 'light',
         sizes: '64x64',
     }, {
-        src: '/favicon/gundam-32.png',
+        src: '/favicon/sword-32.png',
         theme: 'light',
         sizes: '32x32',
     }, {
-        src: '/favicon/gundam-24.png',
-        theme: 'light',
-        sizes: '24x24',
-    }, {
-        src: '/favicon/gundam-16.png',
+        src: '/favicon/sword-16.png',
         theme: 'light',
         sizes: '16x16',
     }
