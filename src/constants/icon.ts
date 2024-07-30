c1153dec9bc2d8e90f1d3bc0199c9a97bc050471
kinho <kinho6119@163.com>
2024-07-30 23:39:17 +0800

    更换icon方向

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,51 +2,51 @@ import type {Favicon} from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: '/favicon/sword-512.png',
+        src: '/favicon/sword-up-512.png',
         theme: 'dark',
         sizes: '512x512',
     }, {
-        src: '/favicon/sword-256.png',
+        src: '/favicon/sword-up-256.png',
         theme: 'dark',
         sizes: '256x256',
     }, {
-        src: '/favicon/sword-128.png',
+        src: '/favicon/sword-up-128.png',
         theme: 'dark',
         sizes: '128x128',
     }, {
-        src: '/favicon/sword-64.png',
+        src: '/favicon/sword-up-64.png',
         theme: 'dark',
         sizes: '64x64',
     }, {
-        src: '/favicon/sword-32.png',
+        src: '/favicon/sword-up-32.png',
         theme: 'dark',
         sizes: '32x32',
     }, {
-        src: '/favicon/sword-16.png',
+        src: '/favicon/sword-up-16.png',
         theme: 'dark',
         sizes: '16x16',
     }, {
-        src: '/favicon/sword-512.png',
+        src: '/favicon/sword-up-512.png',
         theme: 'light',
         sizes: '512x512',
     }, {
-        src: '/favicon/sword-256.png',
+        src: '/favicon/sword-up-256.png',
         theme: 'light',
         sizes: '256x256',
     }, {
-        src: '/favicon/sword-128.png',
+        src: '/favicon/sword-up-128.png',
         theme: 'light',
         sizes: '128x128',
     }, {
-        src: '/favicon/sword-64.png',
+        src: '/favicon/sword-up-64.png',
         theme: 'light',
         sizes: '64x64',
     }, {
-        src: '/favicon/sword-32.png',
+        src: '/favicon/sword-up-32.png',
         theme: 'light',
         sizes: '32x32',
     }, {
-        src: '/favicon/sword-16.png',
+        src: '/favicon/sword-up-16.png',
         theme: 'light',
         sizes: '16x16',
     }
